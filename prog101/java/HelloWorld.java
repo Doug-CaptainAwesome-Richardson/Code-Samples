c5576e0b9ee760b9c17464eb5c5328a100fb61b4
Demios <dcrichardson@unomaha.edu>
2014-10-10 04:35:13 -0500

    first commit

---
+++ prog101/java/HelloWorld.java
@@ -0,0 +1,13 @@
+//This is the quintessential basic program
+//It prints out "Hello World" to the console and that's it
+//These are typically called "Hello World Programs" for that reason
+//Not sure why, tradition I suppose
+//Anyway, you might hear that term again, so here you go.
+
+public class HelloWorld
+{
+	public static void main (String args[])
+	{
+		System.out.println("Hello World");
+	}
+}
